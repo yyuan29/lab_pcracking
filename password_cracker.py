7020d8b4800da96d373535b2b7cc9868966b5246
Yumo Yuan <yyuan29@cmc.edu>
2026-02-27 11:55:14 -0800

    add

--- password_cracker.py
+++ password_cracker.py
@@ -4,14 +4,16 @@ import zlib
 with open('SecLists/Passwords/Leaked-Databases/Ashley-Madison.txt', "r", encoding='latin-1') as f:
     passwords = [line.strip() for line in f]
 
-for i,password in enumerate(passwords):
-    if i%1000==0:
-        print('i=',i,'password=',password)
-    with ZipFile('whitehouse_secrets.zip') as zf:
-        try:
-            zf.extractall(pwd=password.encode('ascii'))
-            print('password=', password)
-            break
-        except Exception:
-            pass
+with ZipFile('whitehouse_secrets.zip') as zf: 
+    for i,password in enumerate(passwords):
+        if i%1000==0:
+            print('i=',i,'password=',password)
+        with ZipFile('whitehouse_secrets.zip') as zf:
+            try:
+                zf.extractall(pwd=password.encode('latin-1'))
+                zf.extractall(pwd=password.encode('ascii'))
+                print('password=', password)
+                break
+            except (RuntimeError, zlib.error):
+                pass
 
